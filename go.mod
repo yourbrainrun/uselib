a3f6706f64732199ab03547f2aa8f2707a1f8b03
wangyx <378739901@qq.com>
2021-11-19 18:59:56 +0800

    test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module uselib
+
+go 1.15
+
+require github.com/yourbrainrun/testlib v1.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yourbrainrun/testlib/my_rand"
+)
+
+func main() {
+	aa := my_rand.GetRand()
+	fmt.Println(aa)
+}
