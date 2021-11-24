cc8105ab308f12445ccefba7938622430e3f187e
wangyx <378739901@qq.com>
2021-11-24 10:25:06 +0800

    debug

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module uselib
 
 go 1.15
 
-require github.com/yourbrainrun/testlib v1.0.2
+require (
+	git.cloudhub.top/server/media-server-agent-go-client v1.0.1 // indirect
+	github.com/yourbrainrun/testlib v1.0.2
+)
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"git.cloudhub.top/server/media-server-agent-go-client/ms_requests"
 	"github.com/yourbrainrun/testlib/my_rand"
 )
 
 func main() {
 	aa := my_rand.GetRand()
 	fmt.Println(aa)
+
+	var test ms_requests.BadRequest
+	fmt.Println(test)
 }
