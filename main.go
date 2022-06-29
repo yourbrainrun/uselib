035aeec552df6e67e81f7f10ee17b13a12888ef3
wangyx <378739901@qq.com>
2022-06-29 18:14:45 +0800

    debug

--- main.go
+++ main.go
@@ -11,14 +11,14 @@ func main() {
 }
 
 func testVcr() {
-	client,_ := vcr.NewClient("19e4425489f5a1", "ebd4c801", "vcr.bj.baidubce.com")
+	client,_ := vcr.NewClient("19e44290f5a1", "ebdcabd4c801", "vcr.bj.baidubce.com")
 	// 配置不进行重试，默认为Back Off重试
 	client.Config.Retry = bce.NewNoRetryPolicy()
 
 	// 配置连接超时时间为30秒
 	client.Config.ConnectionTimeoutInMillis = 30 * 1000
 
-	addr := "http://play.roadofcloud.net/live/0a8070ea0/playlist.m3u8"
+	addr := "rtmp://pea0c5d3dd37b"
 	err := client.SimplePutMedia(addr, "test ", "default", "vcr_callback")
 	if err != nil {
 		return
